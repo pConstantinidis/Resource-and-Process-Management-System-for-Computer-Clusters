082c42a0bdec1b297e7c6a573266af975e01337d
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-10 15:38:25 +0300

    Here we go again

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("I am nearly pissed off");
+
+        int num = 0;
+        while (num<10) {
+            System.out.println(num);
+        }
     }
 }
