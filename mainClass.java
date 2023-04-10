c956953adcae339f83d03866702a96413ef8a7f3
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-10 16:07:04 +0300

    That is the last test

---
+++ mainClass.java
@@ -0,0 +1,12 @@
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("I am nearly pissed off");
+
+        int num = 0;
+        while (num<10) {
+            System.out.println(num);
+        }
+
+        System.out.println("It wotks!");
+    }
+}
