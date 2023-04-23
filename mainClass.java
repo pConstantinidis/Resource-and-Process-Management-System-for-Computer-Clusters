5e46bd0a136314371bf81d315fa2a6e097c4b9f4
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-23 16:25:54 +0300

    clusterAdmin class

--- mainClass.java
+++ mainClass.java
@@ -1,12 +1,3 @@
-public class mainClass {
-    public static void main(String[] args) {
-        System.out.println("I am nearly pissed off");
-
-        int num = 0;
-        while (num<10) {
-            System.out.println(num);
-        }
-
-        System.out.println("It wotks!");
-    }
+public class clusterAdmin {
+    
 }
