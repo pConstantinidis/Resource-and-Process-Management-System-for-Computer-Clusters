06bffae54612955db1af8d1cf6673de25cfc8de9
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-23 17:03:55 +0300

    Globals

---
+++ lib/utils/Globals.java
@@ -0,0 +1,12 @@
+package lib.utils;
+
+/**
+ * A class that is to be used widely around the project
+ * 
+ * @Purpose Here we declare fundemental logic and model standarts 
+ * @author pConstantinidis
+ */
+public final class Globals {
+    
+    
+}
