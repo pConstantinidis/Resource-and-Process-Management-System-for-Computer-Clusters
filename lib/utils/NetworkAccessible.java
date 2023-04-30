f1fe677d61008ce71c08f87cc5985a462be7780f
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-01 01:43:39 +0300

    Addition of an accesor

--- lib/utils/NetworkAccessible.java
+++ lib/utils/NetworkAccessible.java
@@ -7,12 +7,17 @@ package lib.utils;
  */
 public interface NetworkAccessible {
 
-    /**
-     * Updates the network bandwidth that the VM has access to.
-     * 
-     * @param newBandwidth The total bandwidth that the VM is going to have access to after the update.
-     * @return The network in GBs/sec that the VM had access to before the update.
-     * @throws InputOutOfAdminsStandartsException
-      */
-    int updateBandwidth(int newBandwidth) throws InputOutOfAdminsStandartsException;
+  /**
+   * An accesor for the bandwidth attribute.
+   */
+  int getBandwidth();
+
+  /**
+    * Updates the network bandwidth that the VM has access to.
+    * 
+    * @param newBandwidth The total bandwidth that the VM is going to have access to after the update.
+    * @return The network in GBs/sec that the VM had access to before the update.
+    * @throws InputOutOfAdminsStandartsException
+    */
+  int updateBandwidth(int newBandwidth) throws InputOutOfAdminsStandartsException;
 }
