80f017168d904e11c02700739470a53b2fb3e281
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-30 15:27:44 +0300

    Creation of an interface used in two VM classes

---
+++ lib/utils/NetworkAccessible.java
@@ -0,0 +1,18 @@
+package lib.utils;
+
+/**
+ * A method that describes virtual machines that have access to the web.
+ * 
+ * @author pConstantinidis
+ */
+public interface NetworkAccessible {
+
+    /**
+     * Updates the network bandwidth that the VM has access to.
+     * 
+     * @param newBandwidth The total bandwidth that the VM is going to have access to after the update.
+     * @return The network in GBs/sec that the VM had access to before the update.
+     * @throws InputOutOfAdminsStandartsException
+      */
+    int updateBandwidth(int newBandwidth) throws InputOutOfAdminsStandartsException;
+}
