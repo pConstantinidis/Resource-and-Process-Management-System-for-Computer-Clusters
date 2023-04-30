7fd07a7678975897ec8d7211b5212177f39ac108
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-30 14:19:36 +0300

    Fundemental code for the PlainVM class

--- src/model/PlainVM.java
+++ src/model/PlainVM.java
@@ -1,17 +1,36 @@
 package src.model;
 
+import lib.utils.Globals;
+import lib.utils.InputOutOfAdminsStandartsException;
 import lib.utils.Globals.*;
 
 public class PlainVM extends VirtualMachine {
     private int drive;
 
-    public PlainVM(int cpu, int ram, OS os, int drive) {
-        if (cpu > ClusterAdmin.getAvailableCpu() || ram > ClusterAdmin.getAvailableRam() || os )
-      
+    // Constructor
+    public PlainVM(int cpu, int ram, OS os, int drive) throws InputOutOfAdminsStandartsException {
+        Globals.isCpuValid(cpu);
+        Globals.isRamValid(ram);
+        Globals.isOsValid(os);
+        Globals.isDriveValid(drive);
       
         this.cpu = cpu;
         this.ram = ram;
         this.os = os;
         this.drive = drive;
+    }
+
+    /**
+     * A method that updates the SSD of a VM.
+     * 
+     * @param newDrive The total drive that the VM is going to have after the update.
+     * @return The drive that the VM had access to before the update.
+     */
+    private int updateDrive(int newDrive) throws InputOutOfAdminsStandartsException {
+        Globals.isDriveValid(newDrive);
+
+        int oldDrive = drive;
+        drive = newDrive;
+        return oldDrive;
     }
 }
