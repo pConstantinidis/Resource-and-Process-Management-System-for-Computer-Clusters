579e45d387415029be0f44afbb0cb05e9fd41453
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-04-30 14:29:28 +0300

    Fundemental code for the VmGPU class

--- src/model/VmGPU.java
+++ src/model/VmGPU.java
@@ -1,5 +1,31 @@
 package src.model;
 
+import lib.utils.Globals;
+import lib.utils.InputOutOfAdminsStandartsException;
+import lib.utils.Globals.OS;
+
 public class VmGPU extends PlainVM {
-    
+    private int gpu;
+
+    public VmGPU(int cpu, int ram, OS os, int drive, int gpu) throws InputOutOfAdminsStandartsException {
+        super(cpu, ram, null, drive);
+
+        Globals.isGpuValid(gpu);
+        this.gpu = gpu;
+    }
+
+    /**
+     * A method that updates the GPU of a VM.
+     * 
+     * @param newGpu The total GPU that the VM is going to have access to after the update.
+     * @return The GPU that the VM had access to before the update.
+     * @throws InputOutOfAdminsStandartsException
+     */
+    private int updateGpu(int newGpu) throws InputOutOfAdminsStandartsException {
+        Globals.isGpuValid(newGpu);
+
+        int oldGpu = this.gpu;
+        this.gpu = newGpu;
+        return oldGpu;
+    }
 }
