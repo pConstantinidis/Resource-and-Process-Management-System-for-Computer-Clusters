bd28a8c2e2ab26316ac32a787d592aa38cd6ceee
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-04 18:56:49 +0300

    Add messages to exception throws

--- lib/utils/Globals.java
+++ lib/utils/Globals.java
@@ -44,27 +44,45 @@ public final class Globals {
      */
         public final static void isCpuValid(int newNumOfCores) throws InputOutOfAdminsStandartsException{
             if (newNumOfCores > availableCpu || newNumOfCores < 1)
-                throw new InputOutOfAdminsStandartsException();
+                throw new InputOutOfAdminsStandartsException("invalid CPU request");
         }
 
         public final static void isRamValid(int newRam) throws InputOutOfAdminsStandartsException{
             if (newRam > availableRam || newRam < 1)
-                throw new InputOutOfAdminsStandartsException();
+                throw new InputOutOfAdminsStandartsException("invalid RAM request");
         }
 
         public final static void isDriveValid(int newDrive) throws InputOutOfAdminsStandartsException {
             if (newDrive > availableDrive || newDrive < 1)
-                throw new InputOutOfAdminsStandartsException();
+                throw new InputOutOfAdminsStandartsException("invalid SSD request");
         }
 
         public final static void isGpuValid(int newGpu) throws InputOutOfAdminsStandartsException {
             if (newGpu > availableGpu || newGpu < 1)
-            throw new InputOutOfAdminsStandartsException();
+            throw new InputOutOfAdminsStandartsException("invalid GPU request");
         }
 
         public final static void isBandwidthValid(int newBandwidth) throws InputOutOfAdminsStandartsException {
             if (newBandwidth > availableBandwidth || newBandwidth < 4)
-                throw new InputOutOfAdminsStandartsException();
+                throw new InputOutOfAdminsStandartsException("invalid bandwidth request");
+        }
+
+        /**
+         * @param input The OS to be checked.
+         * @return The enum value corresponding to the {@code input} or null if the input is invalid.
+         */
+        public final static OS isOSValid(String input) {
+            String givenOS = input.toUpperCase();
+
+            if (!givenOS.equals(OS.FEDORA.toString()) && !givenOS.equals(OS.UBUNTU.toString()) && !givenOS.equals(OS.WINDOWS.toString()))
+                return null;
+            
+            switch (givenOS.charAt(0)) {
+                case 'F': return OS.FEDORA;
+                case 'U': return OS.UBUNTU;
+                case 'W': return OS.WINDOWS;
+                default : return null;
+            }
         }
 
 }
