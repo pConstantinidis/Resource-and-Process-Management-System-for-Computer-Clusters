d0de7e6519553ae0ead588a4ea2c3c5da15a844f
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-10 01:36:50 +0300

    Fix the computePriority method + fix imports

--- src/model/clusterAdmin.java
+++ src/model/clusterAdmin.java
@@ -1,44 +1,48 @@
 package src.model;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
-import java.util.Set;
+import java.util.TreeMap;
 
+import lib.Dependencies.BoundedQueue;
+import lib.Dependencies.InputOutOfAdminsStandartsException;
+import lib.Dependencies.NetworkAccessible;
 import lib.utils.Globals;
-import lib.utils.InputOutOfAdminsStandartsException;
-import lib.utils.NetworkAccessible;
 import lib.utils.Globals.OS;
 
 /**
- * 
+ * The class is declared as abstarct to prevent from intstatiating //! Is that ok??
+ * TODO
  * @author pConstantinidis
  */
-public final class ClusterAdmin {
+public abstract class ClusterAdmin {
     public final static int CPU_CORES = 128;          // Cores
     public final static int RAM = 256;                // Gb
     public final static int DRIVE = 2048;             // Gb
-    public final static int GPU = 8;                  // GPUs
+    public final static int GPU = 11;                  // GPUs //!
     public final static int NETWORK_BANDWIDTH = 320;  // Gb/sec
     public final static int MAX_PROGRAM_RUNTIME = 5400; //Seconds
+    public final static int QUEUE_CAPACITY = 15;
 
-    private static Map<Integer, VirtualMachine> clusterVms = new HashMap<>();
+    private static final Map<Integer, VirtualMachine> clusterVms = new TreeMap<>();
     private static int numOfVms = 0;
-    private static Set<Program> clustersPrograms = new HashSet<>(5);
+    private static final LinkedList<Program> clustersPrograms = new LinkedList<>();
+    private static final BoundedQueue<Program> programsInQueue = new BoundedQueue<>(QUEUE_CAPACITY);
 
     // Accessors
-    public int getNumOfVms() {return ClusterAdmin.numOfVms;}
+    public static int getNumOfVms() {return ClusterAdmin.numOfVms;}
     
-    public void createPlainVm(int cpu, int ram, OS os, int drive) throws InputOutOfAdminsStandartsException {
+    public static void addProgram(Program p) {         //! TODO double check
+        clustersPrograms.add(p);
+    }
+
+    public static void createPlainVm(int cpu, int ram, OS os, int drive) throws InputOutOfAdminsStandartsException {
         
         clusterVms.put(numOfVms+1, new PlainVM(cpu, ram, os, drive));
         updateClustersReserve(-cpu, -ram, -drive, 0, 0);
         numOfVms++;
     }
 
-    public static void addProgram(int id , Program p) {         //! TODO double check
-        clustersPrograms.add(p);
-    }
 
     public static void createVmGPU(int cpu, int ram, OS os, int drive, int gpu) throws InputOutOfAdminsStandartsException {
         
@@ -132,16 +136,16 @@ public final class ClusterAdmin {
      * @param vmId For input 0 the method will return a report for all the VMs
      * @return The report is returned in the following formmat: // TODO
      */
-    private static StringBuilder report(int vmId) {
+   // private static StringBuilder report(int vmId) {
 
-        StringBuilder finalReport;
-        if (vmId == 0) {
+   //     StringBuilder finalReport;
+   //     if (vmId == 0) {
 
-        }
-        else if (clusterVms.containsKey(vmId)) {
+    //    }
+    //    else if (clusterVms.containsKey(vmId)) {
 
-        }
-    }
+     //   }
+    //}
 
     /**
      * A method that updates the clusters stock of materials.
@@ -154,6 +158,31 @@ public final class ClusterAdmin {
             Globals.setAvailableBandwidth(Globals.getAvailableBandwidth() + bandwidth);
     }
 
+    private static void loadPrograms() {
+        Program [] array = new Program[clustersPrograms.size()];
+        array = clustersPrograms.toArray(array);
 
+        Globals.Sort.<Program>sort(array);
+        
+        for (Program p:array)
+            programsInQueue.push(p);
+    }
+
+    public static void main(String[] args) throws InputOutOfAdminsStandartsException {
+        ClusterAdmin.createVmNetworkedGpu(42, 64, OS.UBUNTU, 512, 11, 220);
+
+        ClusterAdmin.addProgram(new Program(2, 4, 4, 6, 190, 10));
+        ClusterAdmin.addProgram(new Program(8, 32, 4, 0, 0, 5));
+        ClusterAdmin.addProgram(new Program(12, 8, 128, 0, 16, 3));
+        ClusterAdmin.addProgram(new Program(2, 4, 4, 3, 0, 0));
+        ClusterAdmin.addProgram(new Program(12, 8, 128, 2, 0, 3));
+
+
+        System.out.println(ClusterAdmin.clustersPrograms);
+
+        ClusterAdmin.loadPrograms();
+
+        System.out.println(ClusterAdmin.programsInQueue.toString());
+    }
 
 }
