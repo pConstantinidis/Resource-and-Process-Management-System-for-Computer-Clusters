3ee06cce842acedbb093af1ba6f69f7532ee057a
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-20 20:34:01 +0300

    Addition of better conf file check

--- src/backend/Configuration.java
+++ src/backend/Configuration.java
@@ -31,6 +31,8 @@ public final class Configuration {
      * <blockquote><pre>
      *   os:(String),cores:(Integer),ram:(Integer),ssd:(Integer),bandwidth(or)gpu:(Integer),gpu:(Integer).
      * </pre></blockquote>
+     * 
+     * @Warning In the case that the configuration format differs from the above, unexpected results may occurred.
      *          
      * @return An {@code ArrayList} with the {@code VirtualMachine} objects casted accordingly.
      * @throws IOException
@@ -38,10 +40,9 @@ public final class Configuration {
     private static ArrayList<VirtualMachine> configVMs() throws IOException {
         FileInputStream vms = new FileInputStream("./conf/vms.config");
         BufferedReader fReader = new BufferedReader(new FileReader("./conf/vms.config"));
-        StringTokenizer st;
 
         ArrayList<VirtualMachine> data = new ArrayList<>();
-        String [] line = fReader.readLine().split(",");
+        String [] line = fReader.readLine().trim().split(",");
         String [] specs;
         boolean isGPUed=false, isNetworked=false;
         
@@ -51,9 +52,16 @@ public final class Configuration {
                 specs[i] = line[i].split(":")[1];
             }
 
+            if (line.length == 5) {
+                if (Character.toUpperCase(line[4].charAt(0)) == 'B')
+                    isNetworked = true;
+                else isGPUed = true;
+            }
+            else if (line.length != 4) {                                    //! Takes forgranted that the conf file hase the examples order.
+                if (Integer.parseInt(specs[4]) != 0) isNetworked = true;
+                if (Integer.parseInt(specs[5]) != 0) isGPUed = true;
+            }
             if (Globals.isOSValid(specs[0]) == null) throw new IOException("invalid OS");
-            if (Integer.parseInt(specs[4]) != 0) isNetworked = true;                               //! NEED TO CHECK IF AT THE 5TH PLACE IS GPU OR NETWORK
-            if (Integer.parseInt(specs[5]) != 0) isGPUed = true;
 
             try {
                 if (isGPUed && isNetworked)
@@ -63,23 +71,24 @@ public final class Configuration {
                 else if (isGPUed && !isNetworked)
                     data.add( new
                         VmGPU(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]),
-                        Integer.parseInt(specs[3]), Integer.parseInt(specs[5])));
+                        Integer.parseInt(specs[3]), Integer.parseInt(specs[4])));
                 else if (!isGPUed && isNetworked)
                     data.add( new
                         VmNetworked(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]),
-                        Integer.parseInt(specs[3]), Integer.parseInt(specs[5])))
+                        Integer.parseInt(specs[3]), Integer.parseInt(specs[4])));
                 else
                     data.add( new
                         PlainVM(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]), Integer.parseInt(specs[3])));
             } catch(Exception e) {
                 throw new IOException("invalid VM specs");
             }
-            line = fReader.readLine().split(",");
+            line = fReader.readLine().trim().split(",");
         }
         vms.close();
         fReader.close();
         return data;
     }
+
 
     /**
      * 
@@ -95,12 +104,4 @@ public final class Configuration {
     }
 
 
-    private class vm {
-        
-    }
-
-    private class prg {
-
-    }
-   
 }
