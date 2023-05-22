56f8666e2cde7deef8cee802741b618351d4ba73
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-22 22:26:29 +0300

    Now the basic fundemental methods support out of order inputs

--- src/backend/Configuration.java
+++ src/backend/Configuration.java
@@ -1,19 +1,18 @@
 package src.backend;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
+
+import javax.crypto.IllegalBlockSizeException;
 
 import lib.dependencies.InputOutOfAdminsStandartsException;
 import lib.utils.Globals;
+import lib.utils.Globals.OS;
 import src.model.PlainVM;
+import src.model.Program;
 import src.model.VirtualMachine;
 import src.model.VmGPU;
 import src.model.VmNetworked;
@@ -23,65 +22,56 @@ import src.model.VmNetworkedGPU;
  * TODO
  */
 public final class Configuration {
-    private FileReader fReader;
     
     /**
-     * A static method that reads data from a {@code config} text file and returns a list of the equivilant VMs.
-     * <p>The format of the file must be:
-     * <blockquote><pre>
-     *   os:(String),cores:(Integer),ram:(Integer),ssd:(Integer),bandwidth(or)gpu:(Integer),gpu:(Integer).
-     * </pre></blockquote>
+     * A method that reads data from a {@code config} text file and returns a list of the equivilant VMs.
+     * <p>The format of the file should be:<b>resource:(it's data type),e.t.c.
+     *
+     * <p>Keep in mind that an OS must be defined.
      * 
-     * @Warning In the case that the configuration format differs from the above, unexpected results may occurred.
-     *          
      * @return An {@code ArrayList} with the {@code VirtualMachine} objects casted accordingly.
      * @throws IOException
+     * @throws InputOutOfAdminsStandartsException
+     * @see #configPrograms()
      */
-    private static ArrayList<VirtualMachine> configVMs() throws IOException {
+    public ArrayList<VirtualMachine> configVMs() throws IOException, IllegalArgumentException, InputOutOfAdminsStandartsException{
         FileInputStream vms = new FileInputStream("./conf/vms.config");
         BufferedReader fReader = new BufferedReader(new FileReader("./conf/vms.config"));
 
         ArrayList<VirtualMachine> data = new ArrayList<>();
-        String [] line = fReader.readLine().trim().split(",");
-        String [] specs;
-        boolean isGPUed=false, isNetworked=false;
         
+        OS os=null;
+        int cpu=0, ram=0, drive=0, gpu=0, bandwidth=0;
+        String [] line = fReader.readLine().trim().split(",");        
         while (line != null) {
-            specs = new String[6];
-            for (int i=0; i<line.length; i++) {
-                specs[i] = line[i].split(":")[1];
-            }
-
-            if (line.length == 5) {
-                if (Character.toUpperCase(line[4].charAt(0)) == 'B')
-                    isNetworked = true;
-                else isGPUed = true;
-            }
-            else if (line.length != 4) {                                    //! Takes forgranted that the conf file hase the examples order.
-                if (Integer.parseInt(specs[4]) != 0) isNetworked = true;
-                if (Integer.parseInt(specs[5]) != 0) isGPUed = true;
-            }
-            if (Globals.isOSValid(specs[0]) == null) throw new IOException("invalid OS");
 
             try {
-                if (isGPUed && isNetworked)
-                    data.add( new
-                        VmNetworkedGPU(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]),
-                        Integer.parseInt(specs[3]), Integer.parseInt(specs[5]), Integer.parseInt(specs[4])));
-                else if (isGPUed && !isNetworked)
-                    data.add( new
-                        VmGPU(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]),
-                        Integer.parseInt(specs[3]), Integer.parseInt(specs[4])));
-                else if (!isGPUed && isNetworked)
-                    data.add( new
-                        VmNetworked(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]),
-                        Integer.parseInt(specs[3]), Integer.parseInt(specs[4])));
-                else
-                    data.add( new
-                        PlainVM(Integer.parseInt(specs[1]), Integer.parseInt(specs[2]), Globals.isOSValid(specs[0]), Integer.parseInt(specs[3])));
-            } catch(Exception e) {
-                throw new IOException("invalid VM specs");
+                os = getOS(line);
+                cpu = getCPU(line, true);
+                ram = getRAM(line, true);
+                drive = getDrive(line, true);
+                gpu = getGPU(line, true);
+                bandwidth = getBandwidth(line, true);
+            } catch (Exception e) {
+                vms.close();
+                fReader.close();
+                throw new IllegalArgumentException("invalid resource value");
             }
+
+            switch (concludeType(gpu, bandwidth)) {
+                case 4:
+                    data.add( new VmNetworkedGPU(cpu, ram, os, drive, gpu, bandwidth));
+    
+                case 2:
+                    data.add( new VmGPU(cpu, ram, os, drive, gpu));
+    
+                case 3:
+                    data.add( new VmNetworked(cpu, ram, os, drive, bandwidth));
+    
+                case 1:
+                    data.add( new PlainVM(cpu, ram, os, drive));
+            }
+            
             line = fReader.readLine().trim().split(",");
         }
         vms.close();
@@ -89,18 +79,167 @@ public final class Configuration {
         return data;
     }
 
+    private int getBandwidth(String[] line, boolean isVm) throws NumberFormatException, InputOutOfAdminsStandartsException {
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("bandwidth")) {
+
+                if(isVm) Globals.isGpuValid(Integer.parseInt(resource[1]));
+                else {
+                    if (Integer.parseInt(resource[1]) < 4 || Integer.parseInt(resource[1]) > Globals.getInUseBandwidth())
+                        throw new InputOutOfAdminsStandartsException();
+                }
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        return 0;
+    }
+
+    private int getGPU(String[] line, boolean isVm) throws NumberFormatException, InputOutOfAdminsStandartsException {
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("gpu")) {
+
+                if (isVm) Globals.isGpuValid(Integer.parseInt(resource[1]));
+                else {
+                    if (Integer.parseInt(resource[1]) < 1 || Integer.parseInt(resource[1]) > Globals.getInUseGpu())
+                        throw new InputOutOfAdminsStandartsException();
+                }
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        return 0;
+    }
+
+    private int getDrive(String[] line, boolean isVm) throws NumberFormatException, InputOutOfAdminsStandartsException, IOException {
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("ssd")) {
+
+                if(isVm) Globals.isDriveValid(Integer.parseInt(resource[1]));
+                else {
+                    if (Integer.parseInt(resource[1]) < 1 || Integer.parseInt(resource[1]) > Globals.getInUseDrive())
+                        throw new InputOutOfAdminsStandartsException();
+                }
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        throw new IOException("ssd not found");
+    }
+
+    private int getRAM(String[] line, boolean isVm) throws NumberFormatException, InputOutOfAdminsStandartsException, IOException {
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("ram")) {
+
+                if (isVm) Globals.isRamValid(Integer.parseInt(resource[1]));
+                else {
+                    if (Integer.parseInt(resource[1]) < 1 || Integer.parseInt(resource[1]) > Globals.getInUseRam())
+                        throw new InputOutOfAdminsStandartsException();
+                }
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        throw new IOException("ram not found");
+    }
+
+    private int getCPU(String[] line, boolean isVm) throws NumberFormatException, InputOutOfAdminsStandartsException, IOException {
+
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("cores")) {
+                if (isVm) Globals.isCpuValid(Integer.parseInt(resource[1]));
+                else {
+                    if (Integer.parseInt(resource[1]) < 1 || Integer.parseInt(resource[1]) > Globals.getInUseCpu())
+                        throw new InputOutOfAdminsStandartsException();
+                }
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        throw new IOException("CPU not found");
+    }
+
+    private OS getOS(String[] line) throws IOException {
+
+        String [] resource;
+        OS os;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("os")) {
+                os = Globals.isOSValid(resource[1]);
+                if (os!=null) return os;
+            }
+        }
+        throw new IOException("OS not found");
+    }
+
 
     /**
+     * @return 1 for {@code PlainVM} <p>2 for {@code VmGPU} <p>3 for {@code VmNetworked} <p>4 for {@code VmNetworkedGPU}
+     */
+    private int concludeType(int gpu, int bandwidth) {
+        if (gpu == 0 && bandwidth == 0) return 1;
+        else if (gpu != 0 && bandwidth == 0) return 2;
+        else if (gpu == 0 && bandwidth != 0) return 3;
+        return 4;
+    }
+
+    /**
+     * A method that reads data from a {@code config} text file and returns a list of the equivilant programs.
+     * <p>The format of the file should be:<b>resource:(it's data type),e.t.c.
      * 
-     * @return
+     * @return An {@code ArrayList} with the {@code VirtualMachine} objects casted accordingly.
      * @throws IOException
+     * @throws IllegalBlockSizeException
+     * @throws InputOutOfAdminsStandartsException
      * @see #configVMs()
      */
-    private static String [][] configPrograms() throws IOException {
+    public ArrayList<Program> configPrograms() throws IOException, IllegalBlockSizeException {
         FileInputStream programs = new FileInputStream("./conf/programs.config");
         BufferedReader fReader = new BufferedReader(new FileReader("./conf/programs.config"));
 
+        ArrayList<Program> data = new ArrayList<>();
+        int cpu=0, ram=0, drive=0, gpu=0, bandwidth=0, time=0;
+        String [] line = fReader.readLine().trim().split(",");        
+        while (line != null) {
 
+            try {
+                cpu = getCPU(line, false);
+                ram = getRAM(line, false);
+                drive = getDrive(line, false);
+                gpu = getGPU(line, false);
+                bandwidth = getBandwidth(line, false);
+                time = getExeTime(line);
+            } catch (Exception e) {
+                programs.close();
+                fReader.close();
+                throw new IllegalBlockSizeException("invalid resource value");
+            }
+            data.add(new Program(cpu, ram, drive, gpu, bandwidth, time));
+            
+            line = fReader.readLine().trim().split(",");
+        }
+        programs.close();
+        fReader.close();
+        return data;
+    }
+
+    private int getExeTime(String[] line) throws IOException, NumberFormatException, InputOutOfAdminsStandartsException {
+        String [] resource;
+        for (String str:line) {
+            resource = str.split(":");
+            if (resource[0].toLowerCase().equals("time")) {
+
+                if (Integer.parseInt(resource[1]) <= 0) throw new IllegalArgumentException("illegal execution time value");
+                return Integer.parseInt(resource[1]);
+            }
+        }
+        throw new IOException("execution time not found");
     }
 
 
