efc1618cafc6d4a73dc9970c355c8a9b084595c2
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-05-27 19:34:40 +0300

    In progress

--- src/CLI/CommandLineInterface.java
+++ src/CLI/CommandLineInterface.java
@@ -1,67 +1,67 @@
 package src.CLI;
 
+import lib.utils.Globals;
 import src.controler.CLI_IOHandler;
 
 /**
  * ! ! !    TODO
-    * This class is propably going to be merged with the {@code InputHandler} class and form a CLI class.
-    * The CLI class and the GUI class will communicate there inputs to the {@code ClusterAdmin} class and form there (and not only)
-    * the errors will be thrown and each UI will iplement different error handling
-
+ * This class is propably going to be merged with the {@code InputHandler} class and form a CLI class.
+ * The CLI class and the GUI class will communicate there inputs to the {@code ClusterAdmin} class and form there (and not only)
+ * the errors will be thrown and each UI will iplement different error handling
+ 
  * @author pConstantinidis
  */
 public final class CommandLineInterface extends CLI_IOHandler {
     
     public static void main(String[] args) {
         new CommandLineInterface();
+        System.exit(0);
     }
-
-
+    
+    
     public CommandLineInterface() {
         System.out.println(intro);
         short choice;
         
-        outer: while (true) {
-            if (!admin.vmsAutoConf())           //TODO//! to user test it
-                System.out.println(underLine+"\n\tThere is invalid data among the .conf file\n\nPlease submit them manually...");
-            choice = showMenu();
-            switch (choice) {
-                case 0: 
-                    if (admin.getNumOfVms() == 0) {
-                        System.out.println(underLine+"\n It seems that there aren't currently"
-                            +" any VMs running on the cluster\n In this case you can not proceed.");
+        int fileStat = admin.autoConf();
+        if (fileStat == -1) {
+            outer: while (true) {
+                choice = showMenu();
+                switch (choice) {
+                    case 0: 
+                        if (Globals.areThereAnyVms()) break outer;
                         break;
-                    }
-                    else break outer;
-
-                case 1: createVm();
+                    case 1: createVm();
                     break;
-
-                case 2: updateVm();
+                    
+                    case 2: updateVm();
                     break;
-
-                case 3: deleteVm();
+                    
+                    case 3: deleteVm();
                     break;
-            
-                case 4: report();
+                    
+                    case 4: report();
                     break;
+                }
             }
         }
-
-        System.out.println(introducePrograms);
-        do {
-            acquirProgramData();
-        } while (verify("Do you want to add another program"));
+        if (fileStat==0 || fileStat==-1) {
+            System.out.println(introducePrograms);
+            do {
+                System.out.println(underLine+"\nProgram added succesfuly with ID: "+acquirProgramData()+"\n"+underLine);
+            } while (verify("Do you want to add another program"));
+        }
         
         admin.queuePrograms();
-
-        admin.loadProgram();
-        while (!admin.isQueueEmpty()) {
-            admin.updateRunningPrograms();
-
+        
+        int programsFinished = 0;
+        while (!admin.isQueueEmpty() || programsFinished < admin.getNumOfPrgs()) {
+            
             admin.loadProgram();
+            programsFinished += admin.updateRunningPrograms();
+            
+            
         }
-
+        System.out.println(doubleLine+"\n\tSession ended : "+programsFinished+" programs where executed.\n"+doubleLine+"\n");
     }
-    
 }
