239cce3b6f7906a8fad71d55fe624e33cfd0531c
Panagiotis Constantinidis <panagiotis8924@gmail.com>
2023-06-12 07:21:57 +0300

    Constructor broke down into methods

--- src/CLI/CommandLineInterface.java
+++ src/CLI/CommandLineInterface.java
@@ -1,13 +1,9 @@
 package src.CLI;
 
 import lib.utils.Globals;
-import src.controler.CLI_IOHandler;
 
 /**
- * ! ! !    TODO
- * This class is propably going to be merged with the {@code InputHandler} class and form a CLI class.
- * The CLI class and the GUI class will communicate there inputs to the {@code ClusterAdmin} class and form there (and not only)
- * the errors will be thrown and each UI will iplement different error handling
+ * An uper level class from which the CLI starts to execute.
  
  * @author pConstantinidis
  */
@@ -19,56 +15,82 @@ public final class CommandLineInterface extends CLI_IOHandler {
     }
     
     
+    int fileStat = -1;
+
+    /**
+     * @param isConfigured -1 if is not, 0 if only the VMs are and 1 is it is completly configured.
+     */
     public CommandLineInterface() {
         System.out.println(intro);
-        short choice;
-        
-        int fileStat = admin.autoConf();
-        if (fileStat == -1) {
-            outer: while (true) {
-                choice = showMenu();
-                switch (choice) {
-                    case 0: 
-                        if (Globals.areThereAnyVms()) break outer;
-                        break;
-                    case 1: createVm();
-                        break;
+
+        autoConfigure();
+        displayMenu();
+        createPrgsManualy();
+        processPrgs();                
+    }
+
+    
+    private void processPrgs() {
+        if (fileStat == 1) {
+            admin.queuePrograms();
+                
+            int programsFinished = 0, loadStatus = 0, programsDismissed = 0;
+            while (programsFinished < admin.getNumOfPrgs()-programsDismissed) {
+                
+                while (!admin.isQueueEmpty()) {
+                    loadStatus = admin.loadProgram();
+                    if (loadStatus != 0) break;
                     
-                    case 2: updateVm();
-                        break;
-                    
-                    case 3: deleteVm();
-                        break;
-                    
-                    case 4: report();
-                        break;
                 }
+                if (loadStatus == -1) {
+                    programsDismissed++;
+                    loadStatus = 1;
+                }
+                programsFinished += admin.updateRunningPrograms();
             }
+            System.out.println(doubleLine+"\n\tSession ended : "+programsFinished+" programs where executed.\n"+doubleLine+"\n");
+            closeReader();
         }
-        if (fileStat==0 || fileStat==-1) {
+    }
+
+
+    private void createPrgsManualy() {
+        if (fileStat==0 || fileStat==-1) {              //!TODO Is that statment enough?
             System.out.println(introducePrograms);
             do {
                 System.out.println(underLine+"\nProgram added succesfuly with ID: "+acquirProgramData()+"\n"+underLine);
             } while (verify("Do you want to add another program"));
         }
-        
-        admin.queuePrograms();
-
-        int programsFinished = 0, loadStatus = 0, programsDismissed = 0;
-        while (programsFinished < admin.getNumOfPrgs()-programsDismissed) {
-
-            while (!admin.isQueueEmpty()) {
-                loadStatus = admin.loadProgram();
-                if (loadStatus != 0) break;
-                
-            }
-            if (loadStatus == -1) {
-                programsDismissed++;
-                loadStatus = 1;
-            }
-            programsFinished += admin.updateRunningPrograms();
-        }
-        System.out.println(doubleLine+"\n\tSession ended : "+programsFinished+" programs where executed.\n"+doubleLine+"\n");
-        closeReader();
+        fileStat = 1;
     }
+
+    private void autoConfigure() {
+        fileStat = admin.autoConf();
+    }
+    
+    private void displayMenu() {
+        short choice;
+        if (fileStat == -1) {
+            outer: while (true) {
+                choice = showMenu();
+                switch (choice) {
+                    case 0: 
+                    if (Globals.areThereAnyVms()) break outer;
+                        break;
+                    case 1: createVm();
+                        break;
+                    case 2: updateVm();
+                        break;
+                            
+                    case 3: deleteVm();
+                        break;
+                        
+                    case 4: report();
+                        break;
+                }
+            }
+            fileStat = 0;
+        }
+    }
+
 }
